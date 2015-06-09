79af79abdadba23af2554978c6e61ec79593b05c
huhanze <527419134@qq.com>
2015-06-10 00:37:46 +0800

    测试

---
+++ MyFirstDemo/MyFirstDemo/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  MyFirstDemo
+//
+//  Created by 胡海波 on 15/6/10.
+//  Copyright (c) 2015年 胡海波. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    NSLog(@"hello");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
